28949dacdf3a4256b64d4f7d458810832802460f
Ihor Krysenko <ihorkr@wix.com>
2025-05-12 11:39:42 +0300

    Lab 6. Spring security

---
+++ src/main/java/edu/duan/app/docs/SecurityConfig.java
@@ -0,0 +1,48 @@
+package edu.duan.app.docs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/documents/*").hasRole("USER")
+                        .requestMatchers(HttpMethod.POST, "/documents").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT, "/documents").hasRole("USER")
+                        .requestMatchers(HttpMethod.PUT, "/documents/*/sign").hasRole("USER")
+                        .requestMatchers(HttpMethod.DELETE, "/documents/*").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("authorities");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+
+}
--- src/main/java/edu/duan/app/docs/controller/DocumentsController.java
+++ src/main/java/edu/duan/app/docs/controller/DocumentsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/documents")
 public class DocumentsController {
     private DocumentsService documentsService;
 
